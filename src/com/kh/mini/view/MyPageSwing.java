813800adff081f3b43ce59ce89848f673515b852
KHY0705 <khyaa0705@gmail.com>
2021-05-25 14:14:12 +0900

    수정4

--- src/com/kh/mini/view/MyPageSwing.java
+++ src/com/kh/mini/view/MyPageSwing.java
@@ -16,9 +16,7 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JScrollPane;
 import javax.swing.JTable;
-import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 
 import com.kh.mini.controller.AppManager;
